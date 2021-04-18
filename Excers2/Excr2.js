19531eadcf4a0b01a547ed493721c4c013280ea3
Mariam Jangveladze <marryjangveladze@gmail.com>
2021-04-18 21:23:00 +0400

    new change

---
+++ Excers2/Excr2.js
@@ -0,0 +1,25 @@
+const registration = confirm("Do you want to create account?");
+const yesreg = "Yes";
+const noreg = "No";
+
+if (registration) {
+    console.log(yesreg);
+    const fname = prompt(
+        'Enter Your First Name ')
+    const lname = prompt(
+        "Enter Your Last Name"
+    )
+    const age = prompt (" Enter Your Age")
+     if (age <18){
+        const denide = alert ("You can not sign up")
+     } else { 
+        const Email=prompt(
+            "Enter Your E-mail Adress"
+        )
+        const congr = alert ( "Hello" + "  " + fname + "  " + lname + "!  " + "Congrats with registration!" )
+
+    }
+   
+} else{
+    const norepl = alert ("You denied Registration!");
+}
